d03d98173b2a9a0790914b4403c78fc2d343848d
Marc Barbeau <mabarbeau1@gmail.com>
2020-09-07 10:34:49 -0600

    Move test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
-  transform: {
-    "^.+\\.vue$": "vue-jest"
-  }
+  testMatch: ["**/*.spec.(js|jsx|ts|tsx)"],
+  transform: { 
+    "^.+\\.vue$": "vue-jest",
+  },
 };
