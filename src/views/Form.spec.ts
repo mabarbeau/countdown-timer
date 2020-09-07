6d71943c900f778066cbf08946065157ebc38579
Marc Barbeau <mabarbeau1@gmail.com>
2020-09-07 10:47:29 -0600

    Create form view

---
+++ src/views/Form.spec.ts
@@ -0,0 +1,9 @@
+import { shallowMount } from "@vue/test-utils";
+import Timer from "@/views/Form.vue";
+
+describe("Form.vue", () => {
+  it("renders a form", () => {
+    const wrapper = shallowMount(Timer) 
+    expect(wrapper.text()).toBe("Form");
+  });
+});
