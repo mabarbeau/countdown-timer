f81c14c96a5242de886a7cf4f9cddfb5901220e5
Marc Barbeau <mabarbeau1@gmail.com>
2020-09-08 14:17:58 -0600

    Fix test

--- src/views/Form.spec.ts
+++ src/views/Form.spec.ts
@@ -1,9 +1,9 @@
 import { shallowMount } from "@vue/test-utils";
-import Timer from "@/views/Form.vue";
+import Form from "@/views/Form.vue";
 
 describe("Form.vue", () => {
   it("renders a form", () => {
-    const wrapper = shallowMount(Timer);
+    const wrapper = shallowMount(Form);
     expect(wrapper.text()).toBe("Form");
   });
 });
