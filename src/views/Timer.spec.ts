a2bf0e4c7aa1ae2de2b910b63582d3c4a63dae6b
Marc Barbeau <mabarbeau1@gmail.com>
2020-09-08 14:17:17 -0600

    Create timer

--- src/views/Timer.spec.ts
+++ src/views/Timer.spec.ts
@@ -1,9 +1,19 @@
 import { shallowMount } from "@vue/test-utils";
 import Timer from "@/views/Timer.vue";
+import moment from "moment";
+
+jest.mock("moment", () => () => ({
+  format: () => "2020–01–01T01:00:00+00:00"
+}));
+jest.useFakeTimers();
 
 describe("Timer.vue", () => {
   it("renders a timer", () => {
-    const wrapper = shallowMount(Timer);
-    expect(wrapper.text()).toBe("Timer");
+    const wrapper = shallowMount(Timer, {
+      props: {
+        date: moment(1).format()
+      }
+    });
+    expect(wrapper.text()).toMatchInlineSnapshot(`""`);
   });
 });
--- src/views/Timer.vue
+++ src/views/Timer.vue
@@ -1,5 +1,84 @@
 <template>
+  <h1 v-if="$route.query.title">
+    {{ $route.query.title }}
+  </h1>
   <div>
-    Timer
+    <dl v-if="remaining">
+      <dt v-if="remaining.years">Year{{ s(remaining.years) }}</dt>
+      <dd v-if="remaining.years">{{ remaining.years }}</dd>
+
+      <dt v-if="remaining.months">Month{{ s(remaining.months) }}</dt>
+      <dd v-if="remaining.months">{{ remaining.months }}</dd>
+
+      <dt v-if="remaining.days">Day{{ s(remaining.days) }}</dt>
+      <dd v-if="remaining.days">{{ remaining.days }}</dd>
+
+      <dt v-if="remaining.hours">Hour{{ s(remaining.hours) }}</dt>
+      <dd v-if="remaining.hours">{{ remaining.hours }}</dd>
+
+      <dt v-if="remaining.minutes">Minute{{ s(remaining.minutes) }}</dt>
+      <dd v-if="remaining.minutes">{{ remaining.minutes }}</dd>
+
+      <dt>Second{{ s(remaining.seconds) }}</dt>
+      <dd>{{ remaining.seconds }}</dd>
+    </dl>
   </div>
 </template>
+
+<script>
+import { defineComponent } from "vue";
+import moment from "moment";
+
+export default defineComponent({
+  name: "Timer",
+  props: {
+    date: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      now: null,
+      remaining: null
+    };
+  },
+  mounted() {
+    this.tick();
+    this.tock();
+  },
+  beforeUnmount() {
+    window.clearTimeout(this.$options.timer);
+  },
+  methods: {
+    s(count) {
+      if (count !== 1) return "s";
+    },
+    tick(timeout = 50) {
+      this.$options.timer = window.setTimeout(this.updateDateTime, timeout);
+    },
+    tock(timeout = 30 * 60) {
+      this.$options.timer = window.setTimeout(
+        this.updateHumanReadable,
+        timeout
+      );
+    },
+    updateHumanReadable() {
+      const title = moment(this.date).fromNow();
+      document.title = title.charAt(0).toUpperCase() + title.slice(1);
+    },
+    updateDateTime() {
+      const duration = moment.duration(moment(this.date).diff(moment()));
+      this.remaining = {
+        years: duration.years(),
+        months: duration.months(),
+        days: duration.days(),
+        hours: duration.hours(),
+        minutes: duration.minutes(),
+        seconds: duration.seconds()
+      };
+      this.tick();
+    }
+  }
+});
+</script>
