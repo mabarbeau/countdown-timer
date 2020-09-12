ada19039e8fa2125abda31e6df092bb8af1f8ef6
Marc Barbeau <mabarbeau1@gmail.com>
2020-09-12 11:43:34 -0600

    Fix test

--- src/views/Timer.spec.ts
+++ src/views/Timer.spec.ts
@@ -1,18 +1,25 @@
 import { shallowMount } from "@vue/test-utils";
 import Timer from "@/views/Timer.vue";
-import moment from "moment";
 
-jest.mock("moment", () => () => ({
-  format: () => "2020–01–01T01:00:00+00:00"
-}));
+jest.spyOn(Date, "now").mockImplementationOnce(() => 1599932495); 
+
 jest.useFakeTimers();
 
 describe("Timer.vue", () => {
   it("renders a timer", () => {
+    jest.runOnlyPendingTimers();
     const wrapper = shallowMount(Timer, {
       props: {
-        date: moment(1).format()
-      }
+        date: "2020-10-10T17:18:12Z",
+      },
+      global: {
+        mocks: {
+          $route: {
+            params: {},
+            query: {},
+          },
+        },
+      },
     });
     expect(wrapper.text()).toMatchInlineSnapshot(`""`);
   });
